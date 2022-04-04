518280e41baa0b57130d3a6379d4343fae1f13b6
mackan253 <75488904+mackan253@users.noreply.github.com>
2022-04-04 14:29:04 +0200

    added some endpoints and cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,49 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
 const app = express()
+const mysql = require("mysql")
 
-app.get("/", (req, res) =>{
-    res.send("Hello world")
-})
 
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "sqluser",
+    password:"password",
+    database:"carshop_db"
+});
+
+
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+
+app.get("/api/get/employees", (req, res) => {
+    const sqlSelect = "SELECT * FROM employees"
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });
+});
+
+app.get("/api/get/carmodels", (req, res) => {
+    const sqlSelect = "SELECT * FROM carmodels"
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });
+});
+
+app.post("/api/insert", (req, res)=> {
+    const carModel = req.body.carModel
+
+   const sqlInsert = 
+   "INSERT INTO movie_reviews (carmodels) VALUES (?)"
+    db.query(sqlInsert, carModel, (err, result) => {
+        console.log(err);
+    });
+});
 
 app.listen(3001, () => {
-    console.log("running on port 3001")
+    console.log("running on port 301")
 });
